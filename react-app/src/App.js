f7ad5b4dd695c04cea0ed8f3b9b146edf45fa02f
marietcht <marie.tranchant@maif.fr>
2024-05-15 09:47:45 +0200

    change react files

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Too Many Choco
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
