73c696967858ffb35fee48a69fb4fc2cf8b311bf
marietcht <marie.tranchant@maif.fr>
2024-05-15 14:55:27 +0200

    micro changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
-import './assets/styles/App.css';
-import './assets/styles/reserve.css';
-import 'react-calendar/dist/Calendar.css';
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "./assets/styles/App.css";
+import "./assets/styles/reserve.css";
+import "react-calendar/dist/Calendar.css";
 
 function App() {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className='app'>
-      <div className='calendar-container myCustomCalendar'>
+    <div className="app">
+      <div className="calendar-container myCustomCalendar">
         <Calendar onChange={setDate} value={date} />
         {console.log(Calendar)}
       </div>
