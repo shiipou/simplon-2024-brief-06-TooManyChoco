ec78d86f15a4b813af04812193e0457b43c95fad
Omar1H1 <omerhamad1o1@gmail.com>
2024-05-15 14:37:04 +0200

    git resolved

---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import DetailsCard from './Components/DetailsCard';
+
+function App() {
+  return (
+    <DetailsCard></DetailsCard>
+  );
+}
+
+export default App;
