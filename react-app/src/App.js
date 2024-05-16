8208a478a56706a7d5d59db68de25b55e97133ad
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-05-16 16:32:41 +0200

    12 création de chaque sous formulaires composant réutilisable (#55)
    * fix : react installation
    
    * TODO : ameliorer le fetch des types de viennoiserie
    
    * feat : formulaires de creation d'evenement
    
    * fix : update from comments on PR

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,27 @@
+<<<<<<< HEAD
 import React from 'react';
 import NewAccount from './NewAccount/NewAccount.js';
 import './styles/App.css';
+=======
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css';
+import PageFormulaires from './Pages/PageFormulaires';
+>>>>>>> 66c77c85 (12 création de chaque sous formulaires composant réutilisable (#55))
 
 
 function App() {
   return (
+<<<<<<< HEAD
  <div className="App">
       <NewAccount />
     </div>
+=======
+    <BrowserRouter>
+            <Routes>
+              <Route path="/formulaire" element={<PageFormulaires/>} />
+            </Routes>
+    </BrowserRouter>
+>>>>>>> 66c77c85 (12 création de chaque sous formulaires composant réutilisable (#55))
   );
 }
 
