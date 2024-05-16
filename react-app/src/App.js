8adf4476705db2334e1aec85f0ea14abeebad910
marietcht <marie.tranchant@maif.fr>
2024-05-16 11:23:49 +0200

    add comments

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,23 +6,25 @@ import "react-calendar/dist/Calendar.css";
 
 function App() {
   const [date, setDate] = useState(new Date());
+  // création en dur d'évènements réservés pour afficher le design
   const [events, setEvents] = useState([
     new Date("2024-05-09").toDateString(),
     new Date("2024-05-10").toDateString(),
     new Date("2024-05-15").toDateString(),
-    new Date("2024-05-24").toDateString()
+    new Date("2024-05-24").toDateString(),
   ]);
 
   return (
     <div className="app">
       <div className="calendar-container myCustomCalendar">
-        <Calendar onChange={setDate} value={date}
+        {/* utilisation de la méthode Calendar dispo dans React */}
+        <Calendar
+          onChange={setDate}
+          value={date}
           tileClassName={({ date }) =>
-            events.includes(date.toDateString())
-              ? "reserved"
-              : ""
-          } />
-        {console.log(Calendar)}
+            events.includes(date.toDateString()) ? "reserved" : ""
+          }
+        />
       </div>
     </div>
   );
