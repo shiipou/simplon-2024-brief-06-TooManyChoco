da2d1e74f4e9b2f2596866ccf5e648b945433d87
marietcht <marie.tranchant@maif.fr>
2024-05-16 15:22:45 +0200

    add first files

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import LoginForm from "./Components/LoginForm";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/Components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div>
+      <form>
+        <h3>Bonjour</h3>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
