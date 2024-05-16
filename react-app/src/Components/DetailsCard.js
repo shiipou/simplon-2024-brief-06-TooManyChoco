28389457967a08d2ab3c58361846444355453d0d
Omar1H1 <omerhamad1o1@gmail.com>
2024-05-16 10:50:46 +0200

    comments added

--- react-app/src/Components/DetailsCard.js
+++ react-app/src/Components/DetailsCard.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import handleFetch from "../Services/HandleFetch";
-import { FaUser } from "react-icons/fa";
-import { GiCroissant } from "react-icons/gi";
-import "./DetailsCard.css";
+import handleFetch from "../Services/HandleFetch"; // Service pour gérer les requêtes
+import { FaUser } from "react-icons/fa"; // Icône utilisateur
+import { GiCroissant } from "react-icons/gi"; // Icône croissant
+import "./DetailsCard.css"; // Style pour DetailsCard
 
 function DetailsCard() {
-  //vider le useState lors de l'implantation de l'Api
+  // Initialisation des informations de l'événement avec des données factices
   const [eventInfo, setEventInfo] = useState({
     firstName: "Foo Bar",
     date: "2024-05-16",
     viennoiseries: [{ name: "miamiam" }, { name: "nomnom" }],
   });
-  // Array corespondant aux dates du calendrier à modifier en fonction des props reçues
+  // Jours de la semaine à afficher
   const WeekDays = [
     "Dimanche",
     "Lundi",
@@ -21,28 +21,30 @@ function DetailsCard() {
     "Vendredi",
     "Samedi",
   ];
-  // décommenter et mettre en fonction le useEffect
+  // Effet pour charger les données de l'événement (commenté pour le moment)
   useEffect(() => {
     // handleFetch(1).then((data) => {
     //   setUserInfo(data);
     // });
   }, []);
+
   return (
     <>
       <div className="container">
         <div className="content">
-          {/* this is a props coming from the parent component Calender */}
+          {/* Jour de l'événement */}
           <h1>{WeekDays[new Date(eventInfo.date).getDay()]}</h1>
+          {/* Date de l'événement */}
           <p>{eventInfo.date}</p>
           <div className="info">
             <div className="userContainer">
               <FaUser className="userIcon" />
-              {/* dynamic render of user name */}
+              {/* Nom de l'utilisateur */}
               <h2 className="userName">{eventInfo.firstName}</h2>
             </div>
             <div className="foodContainer">
               <GiCroissant className="foodIcon" />
-              {/* dynamic render of food chooise */}
+              {/* Liste des viennoiseries */}
               <ul>
                 {eventInfo?.viennoiseries.map((viennoiserie, index) => (
                   <li key={index}>
