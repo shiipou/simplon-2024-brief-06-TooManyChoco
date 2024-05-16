cc36168588cc11971d7dd310b098d801f33f1878
marietcht <marie.tranchant@maif.fr>
2024-05-16 16:17:16 +0200

    add form

--- react-app/src/Components/LoginForm.js
+++ react-app/src/Components/LoginForm.js
@@ -3,8 +3,30 @@ import React from "react";
 const LoginForm = () => {
   return (
     <div>
+      <h3>Connexion</h3>
       <form>
-        <h3>Bonjour</h3>
+        <label>Adresse</label>
+        <input
+          type="email"
+          name="email"
+          className="form-control"
+          id=""
+          placeholder="Entrez votre adresse email..."
+        />
+        <label>Mot de passe</label>
+        <input
+          type="password"
+          name="password"
+          id="inputPassword5"
+          className="form-control"
+          aria-describedby="Entrez votre mot de passe..."
+        />
+        <div>
+          <button type="submit">Se connecter</button>
+        </div>
+        <div>
+          <a>Mot de passe oublié ?</a>
+        </div>
       </form>
     </div>
   );
