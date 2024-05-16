2aa14f23c8a715c3dbb8e0af476979b213b5988c
Soum <dev.web21@outlook.fr>
2024-05-16 16:39:03 +0200

    ajustement css

--- react-app/src/NewAccount/NewAccount.js
+++ react-app/src/NewAccount/NewAccount.js
@@ -1,28 +1,30 @@
 // NewAccount.js
 import React from 'react';
+import './NewAccount.css';
 
 const NewAccount = () => {
-  return (
-    <div style={{ border: '2px solid red', padding: '20px', borderRadius: '10px', width: '300px' }}>
-      <h2>Créer un compte</h2>
-      <form>
-        <div>
-          <label htmlFor="pseudo">Pseudo :</label>
-          <input type="text" id="pseudo" name="pseudo" />
-        </div>
-        <div>
-          <label htmlFor="email">Email :</label>
-          <input type="email" id="email" name="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Mot de passe :</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <button type="submit" style={{ backgroundColor: 'red', color: 'white' }}>Création de compte</button>
-      </form>
-      <button style={{ backgroundColor: 'red', color: 'white' }}>S'inscrire</button>
-    </div>
-  );
-}
+   return (
+   <div className="main-container">
+     <div className="form-container">
+       <h2>Créer un compte</h2>
+       <form>
+         <div>
+           <label htmlFor="pseudo">Pseudo :</label>
+           <input type="text" id="pseudo" name="pseudo" />
+         </div>
+         <div>
+           <label htmlFor="email">Email :</label>
+           <input type="email" id="email" name="email" />
+         </div>
+         <div>
+           <label htmlFor="password">Mot de passe :</label>
+           <input type="password" id="password" name="password" />
+         </div>
+         <button type="submit">Création de compte</button>
+       </form>
+     </div>
+     </div>
+   );
+ }
 
 export default NewAccount;
