1fcc3f006aff7ca8ef526ec58429339b8dca493a
Soum <dev.web21@outlook.fr>
2024-05-16 16:54:35 +0200

    positionnement bouton creation de compte

--- react-app/src/NewAccount/NewAccount.js
+++ react-app/src/NewAccount/NewAccount.js
@@ -13,6 +13,10 @@ const NewAccount = () => {
            <input type="text" id="pseudo" name="pseudo" />
          </div>
          <div>
+            <label htmlFor="prenom">Prénom :</label>
+             <input type="text" id="prenom" name="prenom" />
+         </div>
+         <div>
            <label htmlFor="email">Email :</label>
            <input type="email" id="email" name="email" />
          </div>
