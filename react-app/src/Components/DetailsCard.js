8809859fd37c00de18dcb00d0bb39757d95243f6
Omar1H1 <omerhamad1o1@gmail.com>
2024-05-16 10:27:44 +0200

    handle Array

--- react-app/src/Components/DetailsCard.js
+++ react-app/src/Components/DetailsCard.js
@@ -5,7 +5,12 @@ import { GiCroissant } from "react-icons/gi";
 import "./DetailsCard.css";
 
 function DetailsCard() {
-  const [userInfo, setUserInfo] = useState([]);
+  //vider le useState lors de l'implantation de l'Api
+  const [eventInfo, setEventInfo] = useState({
+    firstName: "Foo Bar",
+    date: "2024-05-16",
+    viennoiseries: [{ name: "miamiam" }, { name: "nomnom" }],
+  });
   // Array corespondant aux dates du calendrier à modifier en fonction des props reçues
   const WeekDays = [
     "Dimanche",
@@ -16,28 +21,35 @@ function DetailsCard() {
     "Vendredi",
     "Samedi",
   ];
+  // décommenter et mettre en fonction le useEffect
   useEffect(() => {
-    handleFetch(1).then((data) => {
-      setUserInfo(data);
-    });
+    // handleFetch(1).then((data) => {
+    //   setUserInfo(data);
+    // });
   }, []);
   return (
     <>
       <div className="container">
         <div className="content">
           {/* this is a props coming from the parent component Calender */}
-          <h1>{WeekDays[new Date(userInfo.birthDate).getDay()]}</h1>
-          <p>{userInfo.birthDate}</p>
+          <h1>{WeekDays[new Date(eventInfo.date).getDay()]}</h1>
+          <p>{eventInfo.date}</p>
           <div className="info">
             <div className="userContainer">
               <FaUser className="userIcon" />
               {/* dynamic render of user name */}
-              <h2 className="userName">{userInfo.firstName}</h2>
+              <h2 className="userName">{eventInfo.firstName}</h2>
             </div>
             <div className="foodContainer">
               <GiCroissant className="foodIcon" />
               {/* dynamic render of food chooise */}
-              <h2 className="foodName">{userInfo.lastName}</h2>
+              <ul>
+                {eventInfo?.viennoiseries.map((viennoiserie, index) => (
+                  <li key={index}>
+                    <h2 className="foodName">{viennoiserie.name}</h2>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
