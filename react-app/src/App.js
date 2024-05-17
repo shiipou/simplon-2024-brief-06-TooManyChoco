9a3b5dcf976a87f22b65b3b2e2399c7bac47d137
Omer HAMAD <101038977+Omar1H1@users.noreply.github.com>
2024-05-17 08:26:49 +0200

    6 implmentation du front du dtails dun event (#54)
    * something functional
    
    * wdith in auto
    
    * handleFetch
    
    * refactor
    
    * finally
    
    * git resolved
    
    * package json update
    
    * handle Array
    
    * comments added
    
    * index.css removed
    
    * modifcation of naming

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,8 @@
+
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import PageFormulaires from './Pages/PageFormulaires';
+import DetailsCard from "./Components/DetailsCard";
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <BrowserRouter>
             <Routes>
               <Route path="/formulaire" element={<PageFormulaires/>} />
+              <Route path="/details" element={<DetailsCard/>} />
             </Routes>
     </BrowserRouter>
   );
---
+++ react-app/src/Components/DetailsCard.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import handleFetch from "../Services/eventService"; // Service pour gérer les requêtes
+import { FaUser } from "react-icons/fa"; // Icône utilisateur
+import { GiCroissant } from "react-icons/gi"; // Icône croissant
+import "./DetailsCard.css"; // Style pour DetailsCard
+
+function DetailsCard() {
+  // Initialisation des informations de l'événement avec des données factices
+  const [eventInfo, setEventInfo] = useState({
+    firstName: "Foo Bar",
+    date: "2024-05-16",
+    viennoiseries: [{ name: "miamiam" }, { name: "nomnom" }],
+  });
+  // Jours de la semaine à afficher
+  const WeekDays = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
+  // Effet pour charger les données de l'événement (commenté pour le moment)
+  useEffect(() => {
+    // handleFetch(1).then((data) => {
+    //   setUserInfo(data);
+    // });
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          {/* Jour de l'événement */}
+          <h1>{WeekDays[new Date(eventInfo.date).getDay()]}</h1>
+          {/* Date de l'événement */}
+          <p>{eventInfo.date}</p>
+          <div className="info">
+            <div className="userContainer">
+              <FaUser className="userIcon" />
+              {/* Nom de l'utilisateur */}
+              <h2 className="userName">{eventInfo.firstName}</h2>
+            </div>
+            <div className="foodContainer">
+              <GiCroissant className="foodIcon" />
+              {/* Liste des viennoiseries */}
+              <ul>
+                {eventInfo?.viennoiseries.map((viennoiserie, index) => (
+                  <li key={index}>
+                    <h2 className="foodName">{viennoiserie.name}</h2>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DetailsCard;
