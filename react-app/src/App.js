c3305d1c5abe1058d86f8455bf2ed11e322a6117
marietcht <marie.tranchant@maif.fr>
2024-05-17 09:39:33 +0200

    tout fonctionne

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import CalendarContainer from "./Components/CalendarContainer";
 import PageFormulaires from "./Pages/PageFormulaires";
 import DetailsCard from "./Components/DetailsCard";
