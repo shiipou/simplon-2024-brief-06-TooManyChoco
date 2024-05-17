8b178f6deb0587c19f8657d808c0305bb2ad924a
CharlotteCharrier <charlotte.charrier@maif.com>
2024-05-17 10:27:35 +0200

    changed app css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import "./App.css";
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import CalendarContainer from "./Components/CalendarContainer";
