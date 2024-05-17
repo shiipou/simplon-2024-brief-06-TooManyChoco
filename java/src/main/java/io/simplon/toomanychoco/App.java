34a62807e334bab466054092ee6762b97f570fd5
SFaucher26 <158201877+SFaucher26@users.noreply.github.com>
2024-05-17 16:50:42 +0200

    33 gestion des logins api (#65)
    * my changes
    
    * route login if user exist
    
    * "changed firstname"
    
    * Migrator à jour

--- java/src/main/java/io/simplon/toomanychoco/App.java
+++ java/src/main/java/io/simplon/toomanychoco/App.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.sql.*;
 import java.util.concurrent.Executors;
+// import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
 	private static final App instance = new App();
@@ -60,8 +61,7 @@ public class App {
 								() -> new UserNotFoundException(
 										String.format("User '%s' didn't exist in database.", username)));
 
-				// ObjectMapper objectMapper = new ObjectMapper();
-				// String response = user;
+				String response = String.format("Hello, %s", user.getFirstname());
 
 				// request.sendResponseHeaders(200, response.getBytes().length);
 				// request.getResponseBody().write(response.getBytes());
@@ -79,6 +79,42 @@ public class App {
 			}
 		}));
 
+		// Vérifie qu'un user existe en base si oui cette route renvoie les informations d'user
+		server.createContext("/login", (request -> {
+			String method = request.getRequestMethod();
+			if (method.equalsIgnoreCase("POST")) {
+				try {
+					ObjectMapper objectMapper = new ObjectMapper();
+
+					String userInfoJson = new String(request.getRequestBody().readAllBytes());
+					System.out.println(userInfoJson);
+
+					User userInfo = objectMapper.readValue(userInfoJson, User.class);
+					System.out.println(userInfo);
+
+					User user = userRepository
+							.findIsUserExist(userInfo)
+							.orElseThrow(
+									() -> new UserNotFoundException(
+											String.format("User '%s' didn't exist in database.", ((User) userInfo).getEmail())));
+
+					String response = objectMapper.writeValueAsString(user);
+
+					request.sendResponseHeaders(200, response.getBytes().length);
+					request.getResponseBody().write(response.getBytes());
+					request.getResponseBody().close();
+				} catch (com.fasterxml.jackson.core.JsonProcessingException e) {
+					e.printStackTrace();
+				} catch (UserNotFoundException error) {
+					String response = "User not found. ";
+					request.sendResponseHeaders(400, response.getBytes().length);
+					request.getResponseBody().write(response.getBytes());
+					request.getResponseBody().close();
+				}
+			}
+		}));
+
+
 		server.createContext("/event", (request -> {
 			try {
 				// on recherche le contenu dans l'url après /event/ XXXXXX
@@ -91,6 +127,7 @@ public class App {
 										String.format("Event '%s' didn't exist in database.", date)));
 				ObjectMapper objectMapper = new ObjectMapper();
 				String response = objectMapper.writeValueAsString(event);
+
 
 				request.sendResponseHeaders(200, response.getBytes().length);
 				request.getResponseBody().write(response.getBytes());
