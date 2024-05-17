96f0def4e532685debf03cdd7e95e2a1f3c9e188
jbouhet79 <158160064+jbouhet79@users.noreply.github.com>
2024-05-17 14:43:08 +0200

    gestion de identification en dur

--- react-app/src/Services/UserLogin.js
+++ react-app/src/Services/UserLogin.js
@@ -1,6 +1,12 @@
-// avec les users "en dur"
 
-// const users = [{ id: 1, pseudo: 'test', password: 'Test', email: 'test@email.com' }, { id: 2, pseudo: 'user', password: 'Test', email: 'test2@example.com' }]
+
+// avec l'API
+// import { baseApi } from "./_apiConfig"
+
+// export async function getUserById(id) {
+//     return await fetch(new URL(`/users/${id}`, baseApi))
+//         .then(response => response.json())
+// }
 
 // export async function userLogin(email, password) {
 //     return users.find((user) => user.email.toLocaleLowerCase() === email.toLocaleLowerCase() && user.password === password)
@@ -61,3 +67,10 @@ export async function userLogin(email, password) {
 //     })
 //         .then(response => response.json())
 // }
+
+const users = [{id:1, pseudo:'test', password: 'Test', email: 'test@email.com'},{id:2, pseudo:'user', password: 'Test', email: 'test2@example.com'}]
+
+export function userLogin(email, password) {
+    return users.find((user)=>user.email.toLocaleLowerCase() === email.toLocaleLowerCase() && user.password === password)
+}
+
