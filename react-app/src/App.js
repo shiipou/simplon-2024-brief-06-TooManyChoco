f90a50edbe9d3e62efeccb44fd5a17dc8945f9f0
tcht-marie <158160425+tcht-marie@users.noreply.github.com>
2024-05-17 09:40:06 +0200

    feat: Implementation calendrier statique + affiche évènement (#56)
    * avant rebase beta
    
    * i dont know
    
    * Bye Bye
    
    * change react files
    
    * conflits
    
    * add calendaar
    
    * react calendar2
    
    * modif calendar
    
    * css du jour
    
    * police, couleurs, arrondis, position
    
    * Add week-end color
    
    * micro changes
    
    * fix clic bleu
    
    * police, couleurs, arrondis, position3
    
    * change css
    
    * clic bleu fix
    
    * mediaqueries
    
    * corrections
    
    * border white solid weekend
    
    * resolve merge again
    
    * ajout border
    
    * ajout police Roboto
    
    * ajout police Roboto
    
    * retablissement probleme du bleu
    
    * ombre sur le mois
    
    * micro add + nettoyage
    
    * gestion jours reservés
    
    * mise en commun équipe
    
    * corrections 2-app
    
    * correction couleur jour now
    
    * add comments
    
    * Update java/src/main/java/io/simplon/toomanychoco/App.java
    
    Co-authored-by: Shiipou <shiishii@nocturlab.fr>
    
    * Update react-app/package.json
    
    Co-authored-by: Shiipou <shiishii@nocturlab.fr>
    
    * documentation, start, rendre dynamique la reservation
    
    * coorection hoover weekend
    
    * probleme a cause review...
    
    * correction hoover weekend2
    
    * add component CalendarContainer
    
    * changement background
    
    * ajout maquettes calendrier
    
    * pour changer de branch
    
    * icones
    
    * icones
    
    * tout fonctionne
    
    ---------
    
    Co-authored-by: jbouhet79 <jerome.bouhet@maif.fr>
    Co-authored-by: ThibaultMesmin <thibault.mesmin@maif.fr>
    Co-authored-by: Soum <dev.web21@outlook.fr>
    Co-authored-by: jbouhet79 <158160064+jbouhet79@users.noreply.github.com>
    Co-authored-by: Shiipou <shiishii@nocturlab.fr>

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,20 @@
-
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import './App.css';
-import PageFormulaires from './Pages/PageFormulaires';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "./App.css";
+import CalendarContainer from "./Components/CalendarContainer";
+import PageFormulaires from "./Pages/PageFormulaires";
 import DetailsCard from "./Components/DetailsCard";
-
 
 function App() {
   return (
-    <BrowserRouter>
-            <Routes>
-              <Route path="/formulaire" element={<PageFormulaires/>} />
-              <Route path="/details" element={<DetailsCard/>} />
-            </Routes>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CalendarContainer />} />
+          <Route path="/formulaire" element={<PageFormulaires />} />
+          <Route path="/details" element={<DetailsCard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
