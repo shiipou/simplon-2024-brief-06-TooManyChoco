9311a55ebe63206ea97fe6173d83b1d8690232d4
Omer HAMAD <101038977+Omar1H1@users.noreply.github.com>
2024-05-17 08:26:49 +0200

    6 implmentation du front du dtails dun event (#54)
    * something functional
    
    * wdith in auto
    
    * handleFetch
    
    * refactor
    
    * finally
    
    * git resolved
    
    * package json update
    
    * handle Array
    
    * comments added
    
    * index.css removed
    
    * modifcation of naming

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,27 +1,20 @@
-<<<<<<< HEAD
 import React from 'react';
-import NewAccount from './NewAccount/NewAccount.js';
-import './styles/App.css';
-=======
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import PageFormulaires from './Pages/PageFormulaires';
->>>>>>> 66c77c85 (12 création de chaque sous formulaires composant réutilisable (#55))
+import DetailsCard from "./Components/DetailsCard";
+import NewAccount from './NewAccount/NewAccount.js';
 
 
 function App() {
   return (
-<<<<<<< HEAD
- <div className="App">
-      <NewAccount />
-    </div>
-=======
     <BrowserRouter>
             <Routes>
               <Route path="/formulaire" element={<PageFormulaires/>} />
+              <Route path="/details" element={<DetailsCard/>} />
+              <Route path="/register" element={<NewAccount />} />
             </Routes>
     </BrowserRouter>
->>>>>>> 66c77c85 (12 création de chaque sous formulaires composant réutilisable (#55))
   );
 }
 
