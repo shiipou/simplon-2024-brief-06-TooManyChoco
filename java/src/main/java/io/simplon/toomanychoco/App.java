dff553b1c9804fb06be5d5dcd067fd286152cd59
marietcht <marie.tranchant@maif.fr>
2024-05-17 16:44:03 +0200

    add comments java, i need to understand

--- java/src/main/java/io/simplon/toomanychoco/App.java
+++ java/src/main/java/io/simplon/toomanychoco/App.java
@@ -34,7 +34,6 @@ import com.sun.net.httpserver.Headers;
 
 
 public class App {
-
     private static final App instance = new App();
 
     public static App getInstance() {
@@ -42,9 +41,6 @@ public class App {
     }
 
     private final UserRepository userRepository = UserRepository.getInstance();
-    private final EventRespository eventRespository = EventRespository.getInstance();
-    private final PastryRepository pastryRepository = PastryRepository.getInstance();
-    private final EventRepository eventRepository = EventRepository.getInstance();
 
     private App() {
     }
@@ -61,7 +57,7 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
         // Create a basic handler for index route ('/')
-        server.createContext("/", withCORS(request -> {
+        server.createContext("/", (request -> {
             String response = "Hello, world!";
             request.sendResponseHeaders(200, response.getBytes().length);
             request.getResponseBody().write(response.getBytes());
@@ -69,7 +65,7 @@ public class App {
         }));
 
         // Create a new handler with Database access ('/hello/:username')
-        server.createContext("/hello", withCORS(request -> {
+        server.createContext("/hello", (request -> {
             try {
                 String username = request.getRequestURI().getPath().split("/")[2];
 
@@ -79,7 +75,7 @@ public class App {
                                 () -> new UserNotFoundException(
                                         String.format("User '%s' didn't exist in database.", username)));
 
-                String response = String.format("Hello, %s", user.getFirstname());
+                String response = String.format("Hello, %s", user.getFirstName());
 
                 request.sendResponseHeaders(200, response.getBytes().length);
                 request.getResponseBody().write(response.getBytes());
@@ -97,177 +93,11 @@ public class App {
             }
         }));
 
-        // Handler endpoint POST createUser ('/create/user')
-        server.createContext("/create/user", withCORS(request -> {
-            if (request.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
-                request.sendResponseHeaders(204, -1);
-                return;
-            }
-
-            // Lire le corps de la requete sous forme de String
-            String body = new String(request.getRequestBody().readAllBytes());
-            // Parser la String en objet JSON
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                User user = objectMapper.readValue(body, User.class);
-                // Traitement du user
-                userRepository.createUser(user);
-                // Réponse au client
-                request.sendResponseHeaders(201, 0);
-                request.getResponseBody().close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }));
-
-        // Create a new handler with Database access ('/pastries')
-        server.createContext("/pastries", withCORS(request -> {
-            if (request.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
-                request.sendResponseHeaders(204, -1);
-                return;
-            }
-
-            List<Pastry> pastries = pastryRepository.findAll();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String response = objectMapper.writeValueAsString(pastries);
-
-            request.sendResponseHeaders(200, response.getBytes().length);
-            request.getResponseBody().write(response.getBytes());
-            request.getResponseBody().close();
-        }));
-
-        // Create a new handler with Database access ('/events')
-        server.createContext("/events", withCORS(request -> {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String method = request.getRequestMethod();
-
-            if (method.equalsIgnoreCase("OPTIONS")) {
-                request.sendResponseHeaders(204, -1);
-                return;
-            }
-
-            if (method.equalsIgnoreCase("POST")) {
-                try {
-                    String body = new String(request.getRequestBody().readAllBytes());
-                    System.out.println("body : " + body);
-                    Event event = objectMapper.readValue(body, Event.class);
-
-                    // Save the event to the database
-                    eventRepository.save(event, event.getPastries());
-
-                    request.sendResponseHeaders(201, 0);
-                    request.getResponseBody().close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    String response = "Failed to create event";
-                    request.sendResponseHeaders(500, response.getBytes().length);
-                    request.getResponseBody().write(response.getBytes());
-                    request.getResponseBody().close();
-                }
-            } else {
-                request.sendResponseHeaders(405, -1); // Method Not Allowed
-            }
-        }));
-
-        // Vérifie qu'un user existe en base si oui cette route renvoie les informations d'user
-        server.createContext("/login", withCORS(request -> {
-            String method = request.getRequestMethod();
-            if (method.equalsIgnoreCase("OPTIONS")) {
-                request.sendResponseHeaders(204, -1);
-                return;
-            }
-
-            if (method.equalsIgnoreCase("POST")) {
-                try {
-                    ObjectMapper objectMapper = new ObjectMapper();
-
-                    String userInfoJson = new String(request.getRequestBody().readAllBytes());
-                    System.out.println(userInfoJson);
-
-                    User userInfo = objectMapper.readValue(userInfoJson, User.class);
-                    System.out.println(userInfo);
-
-                    User user = userRepository
-                            .findIsUserExist(userInfo)
-                            .orElseThrow(
-                                    () -> new UserNotFoundException(
-                                            String.format("User '%s' didn't exist in database.", userInfo.getEmail())));
-
-                    String response = objectMapper.writeValueAsString(user);
-
-                    request.sendResponseHeaders(200, response.getBytes().length);
-                    request.getResponseBody().write(response.getBytes());
-                    request.getResponseBody().close();
-                } catch (com.fasterxml.jackson.core.JsonProcessingException e) {
-                    e.printStackTrace();
-                } catch (UserNotFoundException error) {
-                    String response = "User not found. ";
-                    request.sendResponseHeaders(400, response.getBytes().length);
-                    request.getResponseBody().write(response.getBytes());
-                    request.getResponseBody().close();
-                }
-            }
-        }));
-
-        server.createContext("/event", withCORS(request -> {
-            try {
-                // Extract the content from the URL after "/event/"
-                String date = request.getRequestURI().getPath().split("/")[2];
-
-                Event event = eventRespository
-                        .findByEventDate(date)
-                        .orElseThrow(() -> new EventNotFoundException(
-                                String.format("Event '%s' didn't exist in the database.", date)));
-
-                // Serialize the event object into a JSON string
-                ObjectMapper objectMapper = new ObjectMapper();
-                String response = objectMapper.writeValueAsString(event);
-
-                // Set CORS headers
-               /*  Headers responseHeaders = httpExchange.getResponseHeaders();
-                responseHeaders.set("Access-Control-Allow-Origin", "*");
-                responseHeaders.set("Access-Control-Allow-Methods", "GET, POST");
-                responseHeaders.set("Access-Control-Allow-Headers", "Content-Type"); */
-
-                // Send the HTTP response
-                 request.sendResponseHeaders(200, response.getBytes().length);
-                request.getResponseBody().write(response.getBytes());
-                request.getResponseBody().close(); 
-            } catch (IndexOutOfBoundsException error) {
-                String response = "Event parameter is missing. Example: `/event/2024-05-16` will return event_date | event_id | first_name | pastry_name.";
-                request.sendResponseHeaders(400, response.getBytes().length);
-                request.getResponseBody().write(response.getBytes());
-                request.getResponseBody().close(); 
-            } catch (EventNotFoundException error) {
-                String response = error.getMessage();
-                request.sendResponseHeaders(404, response.getBytes().length);
-             request.getResponseBody().write(response.getBytes());
-                request.getResponseBody().close();
-            }
-        }));
-
         // Start the server in a new thread
         server.setExecutor(Executors.newFixedThreadPool(numberOfThreads)); // Use a thread pool
         server.start();
 
         System.out.println("Server started on port 8080");
-    }
-
-    private static HttpHandler withCORS(HttpHandler handler) {
-        return exchange -> {
-            Headers headers = exchange.getResponseHeaders();
-            headers.add("Access-Control-Allow-Origin", "*");
-            headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-            headers.add("Access-Control-Allow-Headers", "Content-Type");
-
-            if (exchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
-                exchange.sendResponseHeaders(204, -1);
-                return;
-            }
-
-            handler.handle(exchange);
-        };
     }
 
     public static void main(String[] args) throws SQLException, IOException {
--- java/src/main/java/io/simplon/toomanychoco/repository/UserRepository.java
+++ java/src/main/java/io/simplon/toomanychoco/repository/UserRepository.java
@@ -19,16 +19,7 @@ public class UserRepository {
         return instance;
     }
 
-    // ---------------------------------------------------------------------------------------------------------
-
-    // ---------------------------------------------------------------------------------------------------------
-
     private static final String SQL_FIND_BY_USERNAME = "SELECT * FROM users WHERE username = ?";
-    private static final String SQL_CREATE_USER = "INSERT INTO users (username,first_name,email,password) VALUES (?,?,?,?)";
-
-    // ---------------------------------------------------------------------------------------------------------
-
-    private static final String SQL_FIND_IS_USER_EXIST = "SELECT * FROM users WHERE email = ? AND password = ?";
 
     private Connection connection = null;
 
@@ -57,51 +48,15 @@ public class UserRepository {
             // et encapsulées dans un objet User (new User)
             if (resultSet.next()) {
                 String username = resultSet.getString("username");
-                String firstname = resultSet.getString("first_name");
-                String email = resultSet.getString("email");
-                String password = resultSet.getString("password");
-                return Optional.of(new User(username, firstname, email, password));
+                String firstName = resultSet.getString("first_name");
+                return Optional.of(new User(username, firstName));
             } else {
                 return Optional.empty();
             }
+            // gestion d'erreur
         } catch (SQLException e) {
             e.printStackTrace();
             return Optional.empty();
-        }
-    }
-
-    public Optional<User> findIsUserExist(User user) {
-        try (
-                PreparedStatement statement = connection.prepareStatement(SQL_FIND_IS_USER_EXIST);) {
-            statement.setString(1, user.getEmail());
-            statement.setString(2, user.getPassword());
-            ResultSet resultSet = statement.executeQuery();
-
-            if (resultSet.next()) {
-                String username = resultSet.getString("username");
-                String firstname = resultSet.getString("first_name");
-                String email = resultSet.getString("email");
-                String password = resultSet.getString("password");
-                return Optional.of(new User(username, firstname, email, password));
-            } else {
-                return Optional.empty();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return Optional.empty();
-        }
-    }
-
-    public void createUser(User user) {
-        try (
-                PreparedStatement statement = connection.prepareStatement(SQL_CREATE_USER);) {
-            statement.setString(1, user.getUsername());
-            statement.setString(2, user.getFirstname());
-            statement.setString(3, user.getEmail());
-            statement.setString(4, user.getPassword());
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
