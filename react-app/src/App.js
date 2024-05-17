9789ec0cf4d902b3acbcba302ae21b600a86141d
CharlotteCharrier <158159781+CharlotteCharrier@users.noreply.github.com>
2024-05-17 10:28:28 +0200

    feat: 48 implémentation du design du site css (#57)
    * mise en place ossature react
    
    * mise en forme terminee
    
    * Fin de tache
    
    * Update react-app/package.json
    
    * merge
    
    * changed app css
    
    ---------
    
    Co-authored-by: morganb <morgan.bleunven@maif.fr>
    Co-authored-by: CharlotteCharrier <charlotte.charrier@maif.com>

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import "./App.css";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import CalendarContainer from "./Components/CalendarContainer";
 import PageFormulaires from "./Pages/PageFormulaires";
 import DetailsCard from "./Components/DetailsCard";
@@ -9,12 +10,14 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path="/" element={<CalendarContainer />} />
           <Route path="/formulaire" element={<PageFormulaires />} />
           <Route path="/details" element={<DetailsCard />} />
           <Route path="/register" element={<NewAccount />} />
         </Routes>
+      <Footer />
       </BrowserRouter>
     </div>
   );
