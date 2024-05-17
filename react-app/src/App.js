a89fa6dcb6f467ce12a22c7ddf1f7c218827f663
tcht-marie <158160425+tcht-marie@users.noreply.github.com>
2024-05-17 11:28:00 +0200

    feat: 34 formulaire de login (#58)
    * add first files
    
    * add form
    
    * supp surchage
    
    * new commit
    
    * add submit
    
    * first formulaire
    
    * formulaire centré
    
    * import du login.css
    
    * delete duplicated package-lock.json
    
    ---------
    
    Co-authored-by: jbouhet79 <158160064+jbouhet79@users.noreply.github.com>

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import CalendarContainer from "./Components/CalendarContainer";
 import PageFormulaires from "./Pages/PageFormulaires";
 import DetailsCard from "./Components/DetailsCard";
 import NewAccount from './NewAccount/NewAccount.js';
+import LoginForm from "./Components/LoginForm";
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <BrowserRouter>
-      <Header />
+        <Header />
         <Routes>
           <Route path="/" element={<CalendarContainer />} />
           <Route path="/formulaire" element={<PageFormulaires />} />
           <Route path="/details" element={<DetailsCard />} />
           <Route path="/register" element={<NewAccount />} />
+          <Route path="/login" element={<LoginForm />} />
         </Routes>
-      <Footer />
+        <Footer />
       </BrowserRouter>
     </div>
   );
