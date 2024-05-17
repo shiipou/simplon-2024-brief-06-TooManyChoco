f8a541a487640d3df808d1da4a7c5eb5edb3f981
jbouhet79 <158160064+jbouhet79@users.noreply.github.com>
2024-05-17 16:45:50 +0200

    debut ajout token

--- react-app/src/Services/UserLogin.js
+++ react-app/src/Services/UserLogin.js
@@ -5,3 +5,35 @@ const users = [{id:1, pseudo:'test', password: 'Test', email: 'test@email.com'},
 export function userLogin(email, password) {
     return users.find((user)=>user.email.toLocaleLowerCase() === email.toLocaleLowerCase() && user.password === password)
 }
+
+// avec l'API
+// import { baseApi } from "./_apiConfig"
+
+// export async function getUserById(id) {
+//     return await fetch(new URL(`/users/${id}`, baseApi))
+//         .then(response => response.json())
+// }
+
+// export async function userLogin(email, password) {
+//     return await fetch(new URL('/login', baseApi), {
+//         method: 'get',
+//         headers: {
+//             'content-type': 'application/json',
+//             'accept': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             email,
+//             password
+//         })
+//     })
+//         .then(response => response.json())
+// }
+
+// export async function getUserByToken(token) {
+//     return await fetch(new URL(`/user`, baseApi), {
+//         headers: {
+//             authorization: token
+//         }
+//     })
+//         .then(response => response.json())
+// }
