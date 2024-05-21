35fd7ba442b25f1df9d787849a9895d8adbf5c13
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-05-21 15:16:11 +0200

    fix : doublon dossier migrations nettoyé

--- java/bin/main/migrations/202405171015-alter-users.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-ADD email VARCHAR(255) NOT NULL UNIQUE
-ADD password VARCHAR(255) NOT NULL;
-
-INSERT INTO users (username, firstname, email, password) VALUES ('bob', 'bobo', 'bobo@maif.fr', 'bobo123');
