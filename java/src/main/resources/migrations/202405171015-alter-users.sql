93339e163f119efecd0d0aad98829410f5e0f9b6
morganb75 <158170901+morganb75@users.noreply.github.com>
2024-05-21 13:23:32 +0200

    feat: 28 gestion de creations de comptes utilisateurs dans lapi (#64)
    * first commit methde createUser dans userRepo
    
    * Fin de tache
    
    * remove node_modules
    
    * Remove nodemodule
    
    * Homogeneisation des attributs user
    
    * changed first_name to firstname
    
    ---------
    
    Co-authored-by: CharlotteCharrier <charlotte.charrier@maif.com>

--- java/src/main/resources/migrations/202405171015-alter-users.sql
+++ java/src/main/resources/migrations/202405171015-alter-users.sql
@@ -1,5 +1,5 @@
 ALTER TABLE users
-ADD email VARCHAR(255),
-ADD password VARCHAR(255);
+ADD email VARCHAR(255) NOT NULL UNIQUE
+ADD password VARCHAR(255) NOT NULL;
 
-INSERT INTO users (username, first_name, email, password) VALUES ('bob', 'bobo', 'bobo@maif.fr', 'bobo123');
+INSERT INTO users (username, firstname, email, password) VALUES ('bob', 'bobo', 'bobo@maif.fr', 'bobo123');
