d9033c50ec6531ab266625c4ce8233d7d8103d02
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-05-22 14:40:33 +0200

    78 creation du fetch de la bdd dans les formulaires (#79)
    * Feat : CORS on create event form And fetch pastries from api
    
    * feat : recuperation de la liste des viennoiseries depuis l'api et préparation de la méthode POST pour la creation de l'evenement

--- react-app/src/Services/eventApi.js
+++ react-app/src/Services/eventApi.js
@@ -1,3 +1,4 @@
-export async function createEvent(choix, anonyme) {
-    return await console.log("ce qui est envoyé : " + choix + " " + anonyme)
+export async function createEvent(event_date, choix, anonyme, username) {
+    console.log(choix);
+    return await console.log("ce qui est envoyé : " + event_date + " " + choix + " " + anonyme + " " + username);
     }
