89125aa9ad2c2838a25c4f27456990e0b8a30510
marietcht <marie.tranchant@maif.fr>
2024-05-22 11:53:10 +0200

    merge

--- java/bin/main/migrations/202405171015-alter-users.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-ADD email VARCHAR(255) NOT NULL UNIQUE
-ADD password VARCHAR(255) NOT NULL;
-
-INSERT INTO users (username, first_name, email, password) VALUES ('bob', 'bobo', 'bobo@maif.fr', 'bobo123');
