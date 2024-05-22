bab1149ee1da9a0b3edc6e3e029d6db6845bd116
marietcht <marie.tranchant@maif.fr>
2024-05-22 11:30:15 +0200

    12 création de chaque sous formulaires composant réutilisable (#55)
    * fix : react installation
    
    * TODO : ameliorer le fetch des types de viennoiserie
    
    * feat : formulaires de creation d'evenement
    
    * fix : update from comments on PR

--- java/bin/main/migrations/202405171015-alter-users.sql
+++ java/bin/main/migrations/202405171015-alter-users.sql
@@ -2,4 +2,4 @@ ALTER TABLE users
 ADD email VARCHAR(255) NOT NULL UNIQUE
 ADD password VARCHAR(255) NOT NULL;
 
-INSERT INTO users (username, firstname, email, password) VALUES ('bob', 'bobo', 'bobo@maif.fr', 'bobo123');
+INSERT INTO users (username, first_name, email, password) VALUES ('bob', 'bobo', 'bobo@maif.fr', 'bobo123');
