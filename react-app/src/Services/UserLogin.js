2df1d528a4ffc57dcacd3e9a6c5a4f48c2ef0cc6
jbouhet79 <158160064+jbouhet79@users.noreply.github.com>
2024-05-23 15:33:06 +0200

    feat:  HandleLoginPage (#86)
    * login finally func
    
    * setitem replaced
    
    * = removed

--- react-app/src/Services/UserLogin.js
+++ react-app/src/Services/UserLogin.js
@@ -1,45 +1,31 @@
-// avec les users "en dur"
+import { baseApi } from "./_apiConfig"
 
-const users = [{ id: 1, pseudo: 'test', password: 'Test', email: 'test@email.com' }, { id: 2, pseudo: 'user', password: 'Test', email: 'test2@example.com' }]
+
 
 export async function userLogin(email, password) {
-    return users.find((user) => user.email.toLocaleLowerCase() === email.toLocaleLowerCase() && user.password === password)
-        ? users.find((user) => user.email.toLocaleLowerCase() === email.toLocaleLowerCase() && user.password === password)
-        : alert("error.message");
+    return await fetch(new URL('/login', baseApi), {
+    // return await fetch(new URL('http://localhost:8080/login'), {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+
+    })
+        .then(response => response.json())
+        .catch(error => alert(error.message))
+
 }
 
-// avec l'API
-// import { baseApi } from "./_apiConfig"
-
-// export async function getUserById(id) {
-//     return await fetch(new URL(`/users/${id}`, baseApi))
-//         .then(response => response.json())
-// }
-
-// export async function userLogin(email, password) {
-//     return await fetch(new URL('/login', baseApi), {
-//     // return await fetch(new URL('http://localhost:8080/login'), {
-//         method: 'POST',
-//         headers: {
-//             'content-type': 'application/json',
-//             'accept': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             email,
-//             password
-//         })
-
-//     })
-//         .then(response => response.json())
-//         .catch(error => alert(error.message))
-
-// }
-
-// export async function getUserByToken(token) {
-//     return await fetch(new URL(`/user`, baseApi), {
-//         headers: {
-//             authorization: token
-//         }
-//     })
-//         .then(response => response.json())
-// }
+export async function getUserByToken(token) {
+    return await fetch(new URL(`/user`, baseApi), {
+        headers: {
+            authorization: token
+        }
+    })
+        .then(response => response.json())
+}
