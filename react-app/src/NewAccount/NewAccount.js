4cb8422f6591118c5a93120ee369dcb93647443f
CharlotteCharrier <charlotte.charrier@maif.com>
2024-05-23 10:55:48 +0200

    creation service userCreate

--- react-app/src/NewAccount/NewAccount.js
+++ react-app/src/NewAccount/NewAccount.js
@@ -1,13 +1,41 @@
 // NewAccount.js
-import React from 'react';
+import React, { useCallback } from 'react';
 import './NewAccount.css';
+import { useNavigate } from 'react-router-dom';
 
 const NewAccount = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmitRegister = useCallback(
+    async (e) => {
+      e.preventDefault();
+  
+      //récupérer les données du formulaire
+      const { pseudo, prenom, email, password } = Object.fromEntries(
+        new FormData(e.target)
+      );
+  
+      //que ça les envoie dans l'api pour les stocker dans notre BDD
+      if(pseudo && prenom && email && password) {
+        const newUser = await createUser(pseudo, prenom, email, password);
+        
+        //ça signale que la création a bien été prise en compte
+        
+        //que ça redirige sur le formulaire de login
+        if (newUser) {
+          navigate("/login");
+        }
+      }
+    }
+  
+  )
+
    return (
    <div className="main-container">
      <div className="form-container">
        <h2>Créer un compte</h2>
-       <form className="form-mini-container">
+       <form className="form-mini-container" onSubmit={handleSubmitRegister}>
          <div>
            <label htmlFor="pseudo">Pseudo :</label>
            <input type="text" id="pseudo" name="pseudo" />
