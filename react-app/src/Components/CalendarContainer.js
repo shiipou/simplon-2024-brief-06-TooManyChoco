7928bcf663c5740918a120e215e4a6ed8a010798
jbouhet79 <158160064+jbouhet79@users.noreply.github.com>
2024-05-24 09:50:28 +0200

    feat: HandleNotLogedIn (#90)
    HandleNotLogedIn
    
    Co-authored-by: Omer HAMAD <101038977+Omar1H1@users.noreply.github.com>

--- react-app/src/Components/CalendarContainer.js
+++ react-app/src/Components/CalendarContainer.js
@@ -3,10 +3,20 @@ import Calendar from "react-calendar";
 import "./CalendarContainer.css";
 import "./reserve.css";
 import "react-calendar/dist/Calendar.css";
+import { useNavigate } from "react-router-dom";
+import LoginForm from "./LoginForm";
 
 function CalendarContainer() {
   const [date, setDate] = useState(new Date());
+  const navigate = useNavigate();
   // création en dur d'évènements réservés pour afficher le design
+  const [events, setEvents] = useState([
+    new Date("2024-05-09").toDateString(),
+    new Date("2024-05-10").toDateString(),
+    new Date("2024-05-15").toDateString(),
+    new Date("2024-05-24").toDateString(),
+  ]);
+  sessionStorage.getItem("username") ?? navigate("/");
   const [events, setEvents] = useState([]);
   useEffect(() => {
     fetch('http://localhost:8080/events')
@@ -18,18 +28,18 @@ function CalendarContainer() {
       })
       .catch((error) => console.error("Error fetching events:", error));
   }, []);
-
+  
   return (
-    <div className="calendar-container myCustomCalendar">
-      {/* utilisation de la méthode Calendar dispo dans React */}
-      <Calendar
-        onChange={setDate}
-        value={date}
-        tileClassName={({ date }) =>
-          events.includes(date.toDateString()) ? "reserved" : ""
-        }
-      />
-    </div>
+    (sessionStorage.getItem("username") ? <div className="calendar-container myCustomCalendar">
+    {/* utilisation de la méthode Calendar dispo dans React */}
+    <Calendar
+      onChange={setDate}
+      value={date}
+      tileClassName={({ date }) =>
+        events.includes(date.toDateString()) ? "reserved" : ""
+      }
+    />
+  </div> : <LoginForm />)
   );
 }
 
